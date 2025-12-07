f3f387617d6ef31a6e3957ee151509f8ae35985d
RevNation333 <Info@revnation.co.uk>
2025-12-07 22:55:18 +0000

    Add files via upload

---
+++ RevNation/RevNation/RevNationApp.swift
@@ -0,0 +1,17 @@
+//
+//  RevNationApp.swift
+//  RevNation
+//
+//  Created by user945559 on 12/7/25.
+//
+
+import SwiftUI
+
+@main
+struct RevNationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RevNation/RevNationTests/RevNationTests.swift
@@ -0,0 +1,17 @@
+//
+//  RevNationTests.swift
+//  RevNationTests
+//
+//  Created by user945559 on 12/7/25.
+//
+
+import Testing
+@testable import RevNation
+
+struct RevNationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
